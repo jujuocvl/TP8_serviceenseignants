0f4964ab7d9550bd885bdf39c49d3ea20543dfd7
jujuocvl <julie.orcival@gmail.com>
2023-10-23 17:45:17 +0200

    23/10

--- src/main/java/champollion/Enseignant.java
+++ src/main/java/champollion/Enseignant.java
@@ -1,51 +1,123 @@
 package champollion;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Enseignant extends Personne {
 
-    // TODO : rajouter les autres méthodes présentes dans le diagramme UML
+    private final Set<Intervention> planification = new HashSet<>();
+
+    private final Map<UE, ServicePrevu> enseignements = new HashMap<>();
 
     public Enseignant(String nom, String email) {
         super(nom, email);
     }
 
-    /**
-     * Calcule le nombre total d'heures prévues pour cet enseignant en "heures équivalent TD" Pour le calcul : 1 heure
-     * de cours magistral vaut 1,5 h "équivalent TD" 1 heure de TD vaut 1h "équivalent TD" 1 heure de TP vaut 0,75h
-     * "équivalent TD"
-     *
-     * @return le nombre total d'heures "équivalent TD" prévues pour cet enseignant, arrondi à l'entier le plus proche
-     *
+    public void ajouteIntervention(Intervention inter) {
+		if (!enseignements.containsKey(inter.getMatiere())) {
+			throw new IllegalArgumentException("La matière ne fait pas partie des enseignements");
+		}
+
+		planification.add(inter);
+	}
+
+	public int heuresPlanifiee() {
+		float result = 0f;
+		for (Intervention inter : planification) {
+			result += equivalentTD(inter.getType(), inter.getDuree());
+		}
+
+		return Math.round(result);
+	}
+
+	private float equivalentTD(TypeIntervention type, int volumeHoraire) {
+		float result = 0f;
+		switch (type) {
+			case CM:
+				result = volumeHoraire * 1.5f;
+				break;
+			case TD:
+				result = volumeHoraire;
+				break;
+			case TP:
+				result = volumeHoraire * 0.75f;
+				break;
+		}
+		return result;
+	}
+	
+	public boolean enSousService() {
+		return heuresPrevues() < 192;
+	}
+
+    /*
+     * Calcule le nb total d'heures prévues pour enseignant
+     * 1h CM = 1,5 h "équivalent TD" 
+     * 1h TD = 1h "équivalent TD" 
+     * 1h TP = 0,75h "équivalent TD"
      */
     public int heuresPrevues() {
-        // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        float result = 0;
+        for (UE ue : enseignements.keySet()) {
+			result += heuresPrevuesPourUE(ue);
+		}
+		return Math.round(result);
     }
 
-    /**
-     * Calcule le nombre total d'heures prévues pour cet enseignant dans l'UE spécifiée en "heures équivalent TD" Pour
-     * le calcul : 1 heure de cours magistral vaut 1,5 h "équivalent TD" 1 heure de TD vaut 1h "équivalent TD" 1 heure
-     * de TP vaut 0,75h "équivalent TD"
-     *
-     * @param ue l'UE concernée
-     * @return le nombre total d'heures "équivalent TD" prévues pour cet enseignant, arrondi à l'entier le plus proche
-     *
+    /*
+     * Calcule le nb total d'heures prévues pour enseignant dans l'UE 
      */
     public int heuresPrevuesPourUE(UE ue) {
-        // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        float result = 0;
+
+		ServicePrevu p = enseignements.get(ue);
+		if (p != null) { // La clé existe, l'enseignant intervient dans l'UE
+			result =  equivalentTD(TypeIntervention.CM, p.getVolumeCM())
+				+ equivalentTD(TypeIntervention.TD, p.getVolumeTD())
+				+ equivalentTD(TypeIntervention.TP, p.getVolumeTP())
+			;
+		}
+		return Math.round(result);
     }
 
-    /**
-     * Ajoute un enseignement au service prévu pour cet enseignant
-     *
-     * @param ue l'UE concernée
-     * @param volumeCM le volume d'heures de cours magitral
-     * @param volumeTD le volume d'heures de TD
-     * @param volumeTP le volume d'heures de TP
+    /*
+     * Ajoute un enseignement au service prévu pour enseignant
      */
     public void ajouteEnseignement(UE ue, int volumeCM, int volumeTD, int volumeTP) {
-        // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
-    }
+        if (volumeCM < 0 || volumeTD < 0 || volumeTP < 0) {
+			throw new IllegalArgumentException("Les valeurs doivent être positives ou nulles");
+		}
 
+		ServicePrevu s = enseignements.get(ue);
+		if (s == null) { 
+			s = new ServicePrevu(volumeCM, volumeTD, volumeTP, ue);
+			enseignements.put(ue, s);
+		} else {
+			s.setVolumeCM(volumeCM + s.getVolumeCM());
+			s.setVolumeTD(volumeTD + s.getVolumeTD());
+			s.setVolumeTP(volumeTP + s.getVolumeTP());
+		}
+	}
+      
+    public Set<UE> UEPrevues() {
+		return enseignements.keySet();
+	}
+
+	public int resteAPlanifier(UE ue, TypeIntervention type) {
+		float planifiees = 0f;
+		ServicePrevu p = enseignements.get(ue);
+		if (null == p) 
+			return 0;
+		
+		float aPlanifier = p.getVolumePour(type);
+
+		for (Intervention inter : planification) {
+			if ((ue.equals(inter.getMatiere())) && (type.equals(inter.getType()))) {
+				planifiees += inter.getDuree();
+			}
+		}
+		return Math.round(aPlanifier - planifiees);
+	}
 }
--- src/test/java/champollion/ChampollionJUnitTest.java
+++ src/test/java/champollion/ChampollionJUnitTest.java
@@ -1,40 +1,117 @@
 package champollion;
 
+import java.util.Date;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ChampollionJUnitTest {
-	Enseignant untel;
+	Enseignant enseignant;
 	UE uml, java;
 		
 	@BeforeEach
 	public void setUp() {
-		untel = new Enseignant("untel", "untel@gmail.com");
+		enseignant = new Enseignant("untel", "untel@gmail.com");
 		uml = new UE("UML");
 		java = new UE("Programmation en java");		
+	}
+
+	@Test
+	void ajoutInterventionChangeResteAPlanifier() {
+		// L'enseignant a un service prévu de 10h TD en UML
+		enseignant.ajouteEnseignement(uml, 0, 10, 0);
+		// Rien n'est encore planifié, il reste donc 10h de TD en UML à planifier
+		assertEquals(10, enseignant.resteAPlanifier(uml, TypeIntervention.TD));
+		// On planifie une intervention de 1h TD en UML
+		Intervention inter = new Intervention(1, new Date(), enseignant, uml, TypeIntervention.TD, null);
+		enseignant.ajouteIntervention(inter);
+		// Il reste maintenant 9h TD à planifier en UML
+		assertEquals(9, enseignant.resteAPlanifier(uml, TypeIntervention.TD));
+	}
+
+	@Test
+	void heuresPlanifiees() {
+		assertEquals(0, enseignant.heuresPlanifiee());
+		// L'enseignant a un service prévu de 10h TD en UML
+		enseignant.ajouteEnseignement(uml, 0, 10, 0);
+		// On planifie une intervention de 1h TD en UML
+		Intervention inter = new Intervention(1, new Date(), enseignant, uml, TypeIntervention.TD, null);
+		enseignant.ajouteIntervention(inter);
+		assertEquals(1, enseignant.heuresPlanifiee());
+	}
+
+	@Test
+	void ajoutInterventionNecessiteServicePrevuDansUE() {
+		// L'enseignant a un service prévu de 10h TD en UML
+		enseignant.ajouteEnseignement(uml, 0, 10, 0);
+		// On crée une intervention de 1h en java pour cet enseignant
+		Intervention inter = new Intervention(1, new Date(), enseignant, java, TypeIntervention.TD, null);
+		
+		try {
+			// L'enseignant n'a pas de service prévu en java
+			enseignant.ajouteIntervention(inter);
+			// La ligne ci-dessus DOIT lever une exception
+			// Si on arrive ici c'est une erreur, le test échoue
+			fail("L'enseignant n'a pas de service prévu dans cette UE");
+		} catch (IllegalArgumentException ex) {
+			// Si on arrive ici c'est normal
+		}
 	}
 	
 
 	@Test
 	public void testNouvelEnseignantSansService() {
-		assertEquals(0, untel.heuresPrevues(),
-                        "Un nouvel enseignant doit avoir 0 heures prévues");
+		assertEquals(0, enseignant.heuresPrevuesPourUE(uml));
+		// Un nouvel enseignant n'a pas d'heure au total
+        assertEquals(0, enseignant.heuresPrevues());
+	}
+
+	@Test
+	void nouvelEnseignantSansUE() {
+		assertTrue( enseignant.UEPrevues().isEmpty() );
+	}
+
+	@Test
+	void nouvelEnseignantEnSousService() {
+		assertTrue( enseignant.enSousService() );
 	}
 	
 	@Test
 	public void testAjouteHeures() {
-                // 10h TD pour UML
-		untel.ajouteEnseignement(uml, 0, 10, 0);
+		//Au début, aucune heure prévue
+		assertEquals(0, enseignant.heuresPrevues());
+		enseignant.ajouteEnseignement(uml, 0, 10, 0);
+		// Il a maintenant 10 heures prévues pour cette UE
+		assertEquals(10, enseignant.heuresPrevuesPourUE(uml));
+		enseignant.ajouteEnseignement(uml, 0, 20, 0);
+		// Il a maintenant 10 + 20 heures prévues pour cette UE
+		assertEquals(30, enseignant.heuresPrevuesPourUE(uml));		
+                assertEquals(30, enseignant.heuresPrevues());
+	}
 
-		assertEquals(10, untel.heuresPrevuesPourUE(uml),
-                        "L'enseignant doit maintenant avoir 10 heures prévues pour l'UE 'uml'");
+	@Test
+	void ajoutHeuresNegativesImpossible() {
+		try {
+			// La ligne ci-dessous DOIT lever une exception
+			enseignant.ajouteEnseignement(uml, 0, -1, 0);
+			// Si on arrive ici, c'est une erreur
+			// On force l'échec du test
+			fail("La valeur négative aurait du être refusée");
+		} catch (IllegalArgumentException ex) {
+			// Si on arrive ici c'est normal, c'est ce qu'on veut
+		}
+	}
 
-                // 20h TD pour UML
-                untel.ajouteEnseignement(uml, 0, 20, 0);
-                
-		assertEquals(10 + 20, untel.heuresPrevuesPourUE(uml),
-                         "L'enseignant doit maintenant avoir 30 heures prévues pour l'UE 'uml'");		
+	@Test
+	void calculeCorrectementLeService() {
+		// 10hTD -> 10 équivalent TD
+		enseignant.ajouteEnseignement(uml, 0, 10, 0);
+		// 20hCM -> 30 équivalent TD
+		enseignant.ajouteEnseignement(uml, 20, 0, 0);
+		// 20hTP -> 15 équivalent TD
+		enseignant.ajouteEnseignement(java, 0, 0, 20);
 		
+		int expected = 10 + 30 + 15;
+                assertEquals(expected, enseignant.heuresPrevues());		
 	}
 	
 }
